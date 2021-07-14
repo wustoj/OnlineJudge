f59e229e0b85cbc571e0a92726908ff1d03ee16a
dianhsu <50162344+dianhsu@users.noreply.github.com>
2021-07-14 20:50:36 +0800

    Update languages.py

--- judge/languages.py
+++ judge/languages.py
@@ -189,13 +189,13 @@ _go_lang_config = {
         "max_real_time": 5000,
         "max_memory": 1024 * 1024 * 1024,
         "compile_command": "/usr/bin/go build -o {exe_path} {src_path}",
-        "env": ["GOCACHE=/tmp"]
+        "env": ["GOCACHE=/tmp", "GOPATH=$HOME/.go"]
     },
     "run": {
         "command": "{exe_path}",
         "seccomp_rule": "",
         # 降低内存占用
-        "env": ["GODEBUG=madvdontneed=1"] + default_env,
+        "env": ["GODEBUG=madvdontneed=1", "GOPATH=$HOME/.go"] + default_env,
         "memory_limit_check_only": 1
     }
 }
